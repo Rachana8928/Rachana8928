2b36059dc40ea7e4521b50df0335fd2527e869eb
Rachana8928 <sharmarachna794@gmail.com>
2024-06-05 23:14:22 +0530

    Add files via upload

---
+++ intex.js
@@ -0,0 +1,16 @@
+//variable
+ var yes =true;
+ var no = false
+
+ if (yes)
+ 
+ { alert("this code block will be excuted");
+
+
+ }
+if(no)
+    {
+        alert("this code not bloke will be excuted");
+    }
+
+
